3ff87a8c11d34a76c506e1096a870a86f042d9cb
seoung heon <zkzk8953@naver.com>
2021-08-12 18:46:34 +0900

    11

--- src/component/Map.js
+++ src/component/Map.js
@@ -4,51 +4,119 @@ import GoogleMapReact from 'google-map-react';
 
 import "../asset/css/menu.css";
 
-const MyMarker = ({ text, setHoverSwitch, hoverSwitch, $hover, id}) => <div className={!hoverSwitch ? "circle" : "circle_hover"} onMouseEnter={()=>{setHoverSwitch(true)}} onMouseLeave={()=>{setHoverSwitch(false)}} ><span className="circleText">{text}</span></div>;
+
 
 export default function Map () {
 
 
-  const [hoverSwitch,setHoverSwitch] = useState({
-  
-  });
-  const [locateInfo,setLocateInfo] = useState({
-    lat : "",
-    lng : ""
+  const [hoverSwitch,setHoverSwitch] = useState(false);
+  const [modalSwitch,setModalSwitch] = useState(false);
+
+  const [center, setCenter] = useState({
+      lat: 37.46412549692522,
+      lng: 126.67672094214076
   })
 
-  const [defaultProps, setDefaultProps] = useState({
-    center: {
-      lat: 37.382095,
-      lng: 126.656179
-    },
-    zoom: 15
+  const [points, setPoints] = useState([]);
+
+  let arrayLength = points.length
+
+  const [clickIndex,setClickIndex] = useState(0);
+
+  const [clickPoint,setClickPoint] = useState({
+    id: arrayLength ,
+    title : "",
+    lat: "",
+    lng: "",
+    content: "",
+    checked: false,
   })
 
-  const points = [
-    { id: 1, title: "아이티 타워", lat: 37.380314706090175, lng:126.66573855414347, checked: false},
-    { id: 2, title: "테크노 어린이공원", lat: 37.385404, lng:126.650899, checked: false},
-    { id: 3, title: "캠퍼스 타운역", lat: 37.387748, lng:126.662186, checked: false}
-  ]
   
-  
+
+
+  const onPlusFnc = () => {
+    if(clickPoint.title === null || clickPoint.content === null){
+      alert('써야지')
+    }else{
+      // let copy = [...points]
+      // copy.push(clickPoint)
+      const copy = points.concat(clickPoint)
+      setPoints(copy)
+      setHoverSwitch(false)
+      setModalSwitch(false) 
+      localStorage.setItem('location',JSON.stringify(copy))   
+    }
+  }
+
+  const onRemoveFnc = (number) => {
+    const filterData = points.filter( item => item.id !== number )
+
+    setPoints(filterData)
+    localStorage.setItem("location",JSON.stringify(filterData))
+  }
+
+  const onKeyPress = (e)=>{
+    if(e.key =='Enter'){
+        onPlusFnc();
+    }
+  }
+
+
+  useEffect(() => {
+    const comeData3 = localStorage.getItem('location'); 
+
+    if(comeData3 !== null){
+      const comeData4 = JSON.parse(comeData3)
+      setPoints(comeData4)
+    }
+  },[])
+
+
 
 
     return (
       <div className="container">
-        <div className="map_wrap">
+         {
+         modalSwitch 
+         ? (
+           <>
+            <div className="modal_wrap" onClick={()=>{
+             setModalSwitch(false) 
+             setHoverSwitch(false)
+             }} />
+            <div className="modal_page">
+                <input placeholder="위치명" onChange={(e)=>{setClickPoint({...clickPoint, title : e.target.value})}}/>
+                <input placeholder="설명" onChange={(e)=>{
+                  setClickPoint({...clickPoint, content : e.target.value, id : arrayLength === 0 ? arrayLength : points[arrayLength-1].id + 1})
+                  }}  onKeyPress={onKeyPress} />
+                <button onClick={onPlusFnc}>⭕</button>
+           </div> 
+           </>
+           )
+         : null
+         }  
+         <div className="map_wrap">
+          <div className={hoverSwitch ? "plus_location_on" : "plus_location"} onClick={()=>{
+            setModalSwitch(true)
+          }} >추가</div>
           <GoogleMapReact
             bootstrapURLKeys={{ 
               key: "AIzaSyC-6dS3FLPVZy7S6qd4sbgHChZ-V89ttpY",
               language:"ko",
               region:"KO" 
             }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
+            onClick={(e)=>{
+              setClickPoint({...clickPoint, lat : e.lat, lng: e.lng});
+              setHoverSwitch(true);
+            }}
+            center={center}
+            defaultZoom={12}
           >
+          
           {
             points.map((obj, index)=>(
-              <MyMarker key={obj.id} lat={obj.lat} lng={obj.lng} text={obj.id} setHoverSwitch={setHoverSwitch} hoverSwitch={hoverSwitch} id={obj.id} />
+              <MyMarker center={center} setCenter={setCenter} key={obj.id} lat={obj.lat} lng={obj.lng} text={obj.content} setHoverSwitch={setHoverSwitch} hoverSwitch={hoverSwitch} id={obj.id} obj={obj} setPoints={setPoints} points={points} index={index} />
             ))
           }
           </GoogleMapReact>
@@ -57,8 +125,20 @@ export default function Map () {
           {
             points.map((obj, index)=>{
               return (
-                <div className={obj.checked ? "map_content_hover" : "map_content"} onMouseOver={()=>{}} onMouseLeave={()=>{setHoverSwitch(false)}} key={obj.id} data-value={obj.id} >
+                <div className={obj.checked ? "map_content_hover" : "map_content"} onMouseEnter={()=>{
+                  let copy = [...points];
+                  copy[index].checked = true;
+                  setPoints(copy);
+                  setCenter({...center, lat: obj.lat, lng: obj.lng})
+                }} onMouseLeave={()=>{
+                  let copy = [...points];
+                  copy[index].checked = false;
+                  setPoints(copy);
+                }} key={obj.id} data-value={obj.id} >
                   <h2>{obj.title}</h2>
+                  <button onClick={()=>{
+                    onRemoveFnc(obj.id)
+                  }}>❌</button>
                 </div>
               )
             })
@@ -67,3 +147,25 @@ export default function Map () {
     </div>    
     )
 };
+
+
+
+
+
+const MyMarker = ({  text, id, obj, setPoints, points, index }) => {
+  return (
+<div className={ obj.checked ? "circle_hover" : "circle"} onMouseEnter={()=>{
+  let copy = [...points];
+  copy[index].checked = true;
+  setPoints(copy);
+}} onMouseLeave={()=>{
+  let copy = [...points];
+  copy[index].checked = false;
+  setPoints(copy);
+}}><span className={obj.checked ? "circleText_hover" : "circleText"}>
+  {obj.title}<br/>{text}
+  </span></div>
+  )
+}
+
+
